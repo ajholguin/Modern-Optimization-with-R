9fd6fa37db5e537233860a8b2abbd28d49931e52
Andrew Holguin <ajholguin1@gmail.com>
2018-12-05 14:26:12 -0800

    formatting - ch 3

--- Chapter3/bag-blind.R
+++ Chapter3/bag-blind.R
@@ -1,21 +1,24 @@
 ### bag-blind.R file ###
 
-source("blind.R") # load the blind search methods
-source("functions.R") # load profit(), cost() and sales()
+source("blind.R")       # load the blind search methods
+source("functions.R")   # load profit(), cost() and sales()
 
 # auxiliary function that sets the optimum price for
 # one bag type (D), assuming an independent influence of
 # a particular price on the remaining bag prices:
-ibag=function(D) # D - type of bag
-{ x=1:1000 # price for each bag type
+#     D - type of bag
+ibag <- function(D) {
+  x <- 1:1000   # price for each bag type
+  
   # set search space for one bag:
-  search=matrix(ncol=5,nrow=1000)
-  search[]=1; search[,D]=x 
-  S1=fsearch(search,profit,"max") 
-  S1$sol[D] # best price
+  search <- matrix(ncol = 5, nrow = 1000)
+  search[] <- 1; search[, D] <- x 
+  S1 <- fsearch(search, profit, "max") 
+  S1$sol[D]     # best price
 }
 
 # compute the best price for all bag types:
-S=sapply(1:5,ibag) 
+S <- sapply(1:5, ibag) 
+
 # show the optimum solution:
-cat("optimum s:",S,"f:",profit(S),"\n")
+cat("optimum s:", S, "--> f:", profit(S), "\n")
--- Chapter3/blind.R
+++ Chapter3/blind.R
@@ -5,10 +5,14 @@
 #    FUN - evaluation function
 #    type - "min" or "max"
 #    ... - extra parameters for FUN
-fsearch <- function(search, FUN, type="min", ...) {
-  x <- apply(search, 1, FUN, ...) # run FUN over all search rows
+fsearch <- function(search, FUN, type = "min", ...) {
+  x <- apply(search, 1, FUN, ...)   # run FUN over all search rows
   ib <- switch(type, min = which.min(x), max = which.max(x))
-  return(list(index = ib, sol = search[ib, ], eval = x[ib]))
+  return(
+    list(index = ib,          # index of solution
+         sol = search[ib, ],  # value of solution (in search space)
+         eval = x[ib]         # value of evaluation function at solution
+    ))
 }
 
 # depth-first full search method
@@ -37,7 +41,7 @@ dfsearch <- function(l = 1, b = 1, domain, FUN, type = "min", D = length(domain)
     else
       return(list(sol = x, eval = f))
   } else {
-     # go through sub branches
+    # go through sub branches
     for (j in 1:length(domain[[l]])) {
       x[l] <- domain[[l]][j]
       bcur <- dfsearch(l + 1, j, domain, FUN, type, D = D, x = x, bcur = bcur, ...)
